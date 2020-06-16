9cf865025df896deee6ad87fd01bccc3c86bbea1
sagex360 <sagex360@gmail.com>
2020-06-16 18:51:38 +0300

    feat: Basic routes.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('/dashboard')
+    ->name('dashboard.')
+    ->namespace('Dashboard')
+    ->group(function () {
+
+        Route::resource('files', 'FilesController')
+            ->only(['create', 'edit', 'update', 'destroy', 'store', 'index']);
+
+        Route::get('/', 'HomeController@index')->name('home');
+    });
+
+Route::redirect('/', '/dashboard');
