84e4bcd6073777208ce5e4197406a738610cb764
sagex360 <sagex360@gmail.com>
2020-06-16 20:32:36 +0300

    feat: Dashboard index page.

---
+++ resources/lang/en/texts.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Static Texts Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match all
+    | static texts found on website.
+    |
+    */
+
+    'dashboard' => [
+        'index' => [
+            'header' => 'You are in dashboard!'
+        ]
+    ]
+];
---
+++ resources/views/pages/client/dashboard/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard')
+
+@section('dashboard-content')
+    <div class="container-fluid dashboard-content">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <h2 class="text-center">{{ trans('texts.dashboard.index.header') }}</h2>
+            </div>
+        </div>
+    </div>
+@endsection
