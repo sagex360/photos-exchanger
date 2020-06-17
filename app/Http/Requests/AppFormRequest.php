903ecd23281e21f0aea68cd574a0ba03938139ab
sagex360 <sagex360@gmail.com>
2020-06-17 09:46:56 +0300

    chore: Introduce AppFormRequest to disable request authorization.

---
+++ app/Http/Requests/AppFormRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class AppFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * Returns true, because authorization has to be in another place.
+     *
+     * @return true
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
