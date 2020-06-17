0f4615b8a8fb8feead14c818ed655c119fb4d75a
sagex360 <sagex360@gmail.com>
2020-06-17 17:02:44 +0300

    feat: Dto to register client.

---
+++ app/DTO/Auth/Register/RegisterClientDto.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\DTO\Auth\Register;
+
+
+use App\ValueObjects\Login;
+use App\ValueObjects\Name;
+use App\ValueObjects\Password;
+
+final class RegisterClientDto
+{
+    /** @var Name */
+    protected $name;
+
+    /** @var Login */
+    protected $login;
+
+    /** @var Password */
+    protected $password;
+
+    /**
+     * RegisterClientDto constructor.
+     * @param string $name
+     * @param string $login
+     * @param string $password
+     */
+    public function __construct(string $name, string $login, string $password)
+    {
+        $this->name = Name::create($name);
+        $this->login = Login::create($login);
+        $this->password = Password::create($password);
+    }
+
+    /**
+     * @return Name
+     */
+    public function getName(): Name
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Login
+     */
+    public function getLogin(): Login
+    {
+        return $this->login;
+    }
+
+    /**
+     * @return Password
+     */
+    public function getPassword(): Password
+    {
+        return $this->password;
+    }
+}
