7b70623b5240d9900fc8c1da28b54f2144a4c383
sagex360 <sagex360@gmail.com>
2020-06-23 20:29:42 +0300

    feat: Find by id method.

--- app/Repositories/Files/EloquentFilesRepository.php
+++ app/Repositories/Files/EloquentFilesRepository.php
@@ -5,11 +5,17 @@ namespace App\Repositories\Files;
 
 
 use App\Models\File;
+use Illuminate\Database\Eloquent\Collection;
 
 final class EloquentFilesRepository implements FilesRepository
 {
-    public function overdue()
+    public function overdue(): Collection
     {
         return File::overdue()->get();
+    }
+
+    public function find(int $id): File
+    {
+        return File::findOrFail($id);
     }
 }
--- app/Repositories/Files/FilesRepository.php
+++ app/Repositories/Files/FilesRepository.php
@@ -4,7 +4,19 @@
 namespace App\Repositories\Files;
 
 
+use App\Models\File;
+use Illuminate\Database\Eloquent\Collection;
+
 interface FilesRepository
 {
-    public function overdue();
+    /**
+     * @return Collection|File[]
+     */
+    public function overdue(): Collection;
+
+    /**
+     * @param int $id
+     * @return File
+     */
+    public function find(int $id) : File;
 }
